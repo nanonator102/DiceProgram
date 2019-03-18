4508cd6040a24f9fc3ea3b71871f8f4a8afeac27
nanonator102 <thenanonator102@gmail.com>
2019-03-19 08:26:57 +1100

    Added the ability to print to a file.

--- DiceProgram/src/Main.java
+++ DiceProgram/src/Main.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner; //Class for receiving user input.
 import java.util.Random; //Class for random number generation.
 public class Main {
@@ -8,21 +10,57 @@ public class Main {
         Random rand = new Random();  //Initialization of random number generator
         int i = 0, rollTotal = 0; //Initialization of variables used inside the while loop.
 
-        System.out.println("How many sides would you like to roll?");
+        System.out.print("Would you like to save the results to a text file(1) or print them in the console(2)? ");
+        int saveFile = keyboard.nextInt();
+
+        while(saveFile < 1 || saveFile > 2) //Ensures that saveFile is only 1 or 2,
+        {
+                System.out.print("Please enter either 1 or 2.");
+                saveFile = keyboard.nextInt();
+        }
+
+        System.out.print("How many sides would you like to roll? ");
         int sides = keyboard.nextInt(); //Stores user input integer for the number of sides on the die.
-        System.out.println("How many dice would you like to roll?");
+        System.out.print("How many dice would you like to roll? ");
         int rolls = keyboard.nextInt(); //Stores user input integer for the number of die to roll.
+        String rollCollector[] = new String[rolls];
 
         while (i < rolls) {
 
-            int randNum = rand.nextInt(sides); //Generates a random number between 0 and the number of sides -1.
-            System.out.println("Roll number " + (i+1) + " = " + (randNum+1) ); //Prints the roll number and and the number of the dice face.
-            rollTotal += (randNum+1); //Adds the number generated for the loop to the total count to be output at the end.
             i++; //Increments the variable 'i' by 1.
+            int randNum = rand.nextInt(sides); //Generates a random number between 0 and the number of sides -1.
+            randNum++;
+            if(saveFile==2) {
+                System.out.println("Roll number " + (i) + " = " + randNum ); //Prints the roll number and and the number of the dice face.
+            }
+            rollTotal += (randNum); //Adds the number generated for the loop to the total count to be output at the end.
+            rollCollector[i-1] = ("Roll number " + (i) + " = " + randNum);
 
         }
 
-        System.out.println(rollTotal); //Prints the total count of the dice rolls.
+        i=0;
+        String rollTotalPrint = ("Total roll count = " + rollTotal);
+        if(saveFile==2) {
+            System.out.println(rollTotalPrint); //Prints the total count of the dice rolls.
+        }
+            if (saveFile == 1) {
+                try {
+                    PrintWriter writer = new PrintWriter("Dice.txt");
+                    while(i < rolls) {
+                        writer.println(rollCollector[i]);
+                        i++;
+                    }
+                    writer.println(rollTotalPrint);
+                    writer.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        keyboard.close();
+            if(saveFile==1) {
+                System.out.println("Program executed successfully. Wrote " + rollCollector.length + " rolls to Dice.txt");
+            }
 
         }
 
